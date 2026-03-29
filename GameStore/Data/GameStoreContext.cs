12e51c362255e83aceb364c438f27cc535dad83c
Machias <machias.boschman@gmail.com>
2026-03-29 14:59:56 +0200

    Adding more seed data

--- GameStore/Data/GameStoreContext.cs
+++ GameStore/Data/GameStoreContext.cs
@@ -18,20 +18,24 @@ namespace GameStore.Data
             .HasForeignKey<SteamApp>(s => s.GameId)
             .OnDelete(DeleteBehavior.Cascade);
 
-
+            // Seed Data
             modelBuilder.Entity<Platform>().HasData(
                     new Platform { Id = 1, Name = "PC" },
-                    new Platform { Id = 2, Name = "Playstation"}
+                    new Platform { Id = 2, Name = "Playstation"},
+                    new Platform { Id = 3, Name = "Xbox" }
                 );
             modelBuilder.Entity<Distributor>().HasData(
                     new Distributor { Id = 1, Name = "Steam"},
-                    new Distributor { Id = 2, Name = "Epic Games" }
+                    new Distributor { Id = 2, Name = "Epic Games" },
+                    new Distributor { Id = 3, Name = "Microsoft Store" },
+                    new Distributor { Id = 4, Name = "Playstation" },
+                    new Distributor { Id = 5, Name = "Xbox" }
                 );
             modelBuilder.Entity<Game>().HasData(
-                    new Game { Id = 1, Name = "Call Of Duty", Price = 50, SteamAppId = 1 }
+                    new Game { Id = 1, Name = "Call Of Duty", Price = 50, SteamAppId = 1938090 }
                 );
             modelBuilder.Entity<SteamApp>().HasData(
-                    new SteamApp { Id = 1, GameId = 1 }
+                    new SteamApp { Id = 1938090, GameId = 1 }
                 );
 
             base.OnModelCreating(modelBuilder);
---
+++ GameStore/Migrations/20260329125627_Expanding Seed Data.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GameStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpandingSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SteamApps",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.InsertData(
+                table: "Distributors",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 3, "Microsoft Store" },
+                    { 4, "Playstation" },
+                    { 5, "Xbox" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SteamAppId",
+                value: 1938090);
+
+            migrationBuilder.InsertData(
+                table: "Platforms",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Xbox" });
+
+            migrationBuilder.InsertData(
+                table: "SteamApps",
+                columns: new[] { "Id", "GameId" },
+                values: new object[] { 1938090, 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Distributors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Distributors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Distributors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "SteamApps",
+                keyColumn: "Id",
+                keyValue: 1938090);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SteamAppId",
+                value: 1);
+
+            migrationBuilder.InsertData(
+                table: "SteamApps",
+                columns: new[] { "Id", "GameId" },
+                values: new object[] { 1, 1 });
+        }
+    }
+}
